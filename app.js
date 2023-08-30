3169f201095711be8d5c007637eb5a587a20d467
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-08-30 19:56:28 +0700

    feat: add cors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require('express')
 const router = require('./router')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
